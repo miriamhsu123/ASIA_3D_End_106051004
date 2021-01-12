38e96f68e9ac8a1c48531e09428f7eaeaf22d441
miriamhsu123 <71687830+miriamhsu123@users.noreply.github.com>
2021-01-12 19:53:27 +0800

    0111

---
+++ 3D_End_106051004/Assets/Scipts/Enemy.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using UnityEngine.AI;   // 引用 人工智慧 API
+
+public class Enemy : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 50)]
+    public float speed = 3;
+    [Header("停止距離"), Range(0, 50)]
+    public float stopDistance = 2.5f;
+    [Header("攻擊冷卻時間"), Range(0, 50)]
+    public float cd = 2f;
+    [Header("攻擊中心點")]
+    public Transform atkPoint;
+    [Header("攻擊長度"), Range(0f, 5f)]
+    public float atkLength;
+
+    private Transform player;
+    private NavMeshAgent nav;
+    private Animator ani;
+    /// <summary>
+    /// 計時器
+    /// </summary>
+    private float timer;
+
+    private void Awake()
+    {
+        // 取得身上的元件<代理器>
+        nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+
+        // 尋找其他遊戲物件("物件名稱").變形元件
+        player = GameObject.Find("JunkoChan").transform;
+        // 代理器 的 速度 與 停止距離
+        nav.speed = speed;
+        nav.stoppingDistance = stopDistance;
+    }
+
+    private void Update()
+    {
+        Track();
+        Attack();
+    }
+
+    /// <summary>
+    /// 繪製圖示事件：僅在 Unity 內顯示
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        // 圖示.顏色 = 紅色
+        Gizmos.color = Color.red;
+        // 圖示.繪製射線(中心點，方向)
+        // (攻擊中心點的座標，攻擊中心點的前方 * 攻擊長度)
+        Gizmos.DrawRay(atkPoint.position, atkPoint.forward * atkLength);
+    }
+
+    /// <summary>
+    /// 射線擊中的物件
+    /// </summary>
+    private RaycastHit hit;
+
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void Attack()
+    {
+        if (nav.remainingDistance < stopDistance)
+        {
+            // 時間 累加 (一幀的時間)
+            timer += Time.deltaTime;
+
+            // 取得玩家的座標
+            Vector3 pos = player.position;
+            // 將玩家的座標 Y 軸 指定為 本物件的 Y 軸
+            pos.y = transform.position.y;
+            // 看向(玩家的座標)
+            transform.LookAt(pos);
+
+            // 如果 計時器 >= 冷卻時間 就攻擊 並且計時器歸零
+            if (timer >= cd)
+            {
+                ani.SetTrigger("攻擊觸發");
+                timer = 0;
+
+                // 物理.射線碰撞(攻擊中心點的座標，攻擊中心點的前方，射線擊中的物件，攻擊長度，圖層)
+                // 圖層：1 << 圖層編號
+                // 如果 射線 打到物件 就 執行 {  }
+                if (Physics.Raycast(atkPoint.position, atkPoint.forward, out hit, atkLength, 1 << 8))
+                {
+                    // 碰撞物件.取得元件<玩家>().受傷()
+                    hit.collider.GetComponent<Player>().Damage();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 追蹤
+    /// </summary>
+    private void Track()
+    {
+        // 代理器.設定目的地(玩家的座標)
+        nav.SetDestination(player.position);
+        // 動畫控制器.設定布林值("參數名稱"，剩餘的距離 > 停止距離)
+        ani.SetBool("跑步開關", nav.remainingDistance > stopDistance);
+    }
+}
---
+++ 3D_End_106051004/Assets/Scipts/NPC.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC 資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialogue;
+    [Header("對話內容")]
+    public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval = 0.2f;
+
+    public enum NPCState
+    {
+        FirstDialogue, Missioning, Finish
+    }
+
+    [Header("NPC 狀態")]
+    public NPCState state = NPCState.FirstDialogue;
+
+    /// <summary>
+    /// 玩家是否進入感應區
+    /// </summary>
+    private bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "JunkoChan")
+        {
+            playerInArea = true;
+            StartCoroutine(Dialogue());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "JunkoChan")
+        {
+            playerInArea = false;
+            StopDialogue();
+        }
+    }
+
+    private void StopDialogue()
+    {
+        dialogue.SetActive(false);    // 關閉對話框
+        StopAllCoroutines();        // 停止所有協程
+    }
+
+    /// <summary>
+    /// 開始對話
+    /// </summary>
+    private IEnumerator Dialogue()
+    {
+        /**
+        // print(data.dialogA);            // 取得字串全部資料
+        // print(data.dialogA[0]);         // 取得字串局部資料：語法 [編號]
+
+        // for 迴圈：重複處理相同程式
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    print("我是迴圈：" + i);
+        //}
+
+        //for (int apple = 1; apple < 100; apple++)
+        //{
+        //    print("迴圈：" + apple);
+        //}
+        */
+
+        // 顯示對話框
+        dialogue.SetActive(true);
+        // 清空文字
+        textContent.text = "";
+        // 對話者名稱 指定為 此物件的名稱
+        textName.text = name;
+
+        // 要說的對話
+        string dialogueString = data.dialogueB;
+
+        // 判斷 NPC 狀態 來顯示對應的 對話內容
+        switch (state)
+        {
+            case NPCState.FirstDialogue:
+                dialogueString = data.dialogueA;
+                break;
+            case NPCState.Missioning:
+                dialogueString = data.dialogueB;
+                break;
+            case NPCState.Finish:
+                dialogueString = data.dialogueC;
+                break;
+        }
+
+        // 字串的長度 dialogA.Length
+        for (int i = 0; i < dialogueString.Length; i++)
+        {
+            // print(data.dialogA[i]);
+            // 文字 串聯 
+            textContent.text += dialogueString[i] + "";
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
